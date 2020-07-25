53c8dc5b2711fcc051281e6eded027e4594e18a3
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-25 05:56:21 +0300

    i initially used interrupts to change speed
    But i found a better way to implement the same functionality without interrupts
    Additionaly, I can use the button for changing direction

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,9 +1,13 @@
 //Includes the Arduino Stepper Library
 #include <Stepper.h>
 
-// Defines the number of steps per rotation
+// Defines the number of steps per rotation for the stepper
 const int stepsPerRevolution = 2038;
 #define STEPS 32
+
+int motorSpeed = 200;
+const byte interruptPin = 2;
+int steps = 0;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
@@ -12,17 +16,34 @@ Stepper myStepper = Stepper(STEPS, 8, 10, 9, 11);
 void setup()
 {
     // Nothing to do (Stepper Library sets pins as outputs)
+    Serial.begin(9600);
+    // attachInterrupt(digitalPinToInterrupt(interruptPin), speedChange, CHANGE);
+    int potValue = analogRead(A0);
+    motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
 }
 
 void loop()
 {
-    // Rotate CW slowly
-    myStepper.setSpeed(350);
-    myStepper.step(stepsPerRevolution);
-    delay(1000);
+    int potValue = analogRead(A0);
+    motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
+    Serial.print("PotValue: ");
+    Serial.print(potValue);
+    Serial.print(" :motorSpeed: ");
+    Serial.println(motorSpeed);
 
-    // Rotate CCW quickly
-    myStepper.setSpeed(1000);
-    myStepper.step(-stepsPerRevolution);
-    delay(1000);
+    // Rotate CW slowly
+    myStepper.setSpeed(motorSpeed);
+    myStepper.step(steps);
+    steps = steps + 1;
+    // delay(100);
+
+    // // Rotate CCW quickly
+    // myStepper.setSpeed(motorSpeed);
+    // myStepper.step(-stepsPerRevolution);
+    // delay(100);
 }
+
+// void speedChange()
+// {
+//     motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
+// }
