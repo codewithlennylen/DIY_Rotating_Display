312f5fa7b9d7b38fd1cfc318146c867a6c47f36e
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-25 13:03:46 +0300

    The code Works
    The potentiometer varies the speed of the stepper motor linearly while the button changes the direction of the rotation
    A single 5V power supply is used to power the whole project

--- Code/Code.ino
+++ Code/Code.ino
@@ -5,9 +5,12 @@
 const int stepsPerRevolution = 2038;
 #define STEPS 32
 
-int motorSpeed = 200;
 const byte interruptPin = 2;
+int motorSpeed = 200;
 int steps = 0;
+byte buttonState = 0;
+int potValue;
+byte btnPressed;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
@@ -15,35 +18,53 @@ Stepper myStepper = Stepper(STEPS, 8, 10, 9, 11);
 
 void setup()
 {
-    // Nothing to do (Stepper Library sets pins as outputs)
+    // Nothing to do for stepper pins(Stepper Library sets pins as outputs)
+    pinMode(interruptPin, INPUT);
     Serial.begin(9600);
-    // attachInterrupt(digitalPinToInterrupt(interruptPin), speedChange, CHANGE);
-    int potValue = analogRead(A0);
-    motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
 }
 
 void loop()
 {
-    int potValue = analogRead(A0);
-    motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
-    Serial.print("PotValue: ");
+    // Read the inputs
+    potValue = analogRead(A0);
+    motorSpeed = map(potValue, 0, 712, 200, 1000);
+    btnPressed = digitalRead(interruptPin);
+    if (btnPressed == 1)
+    {
+        directionChange();
+        delay(500);
+    }
+
+    Serial.print("ButtonState: ");
+    Serial.print(buttonState);
+    Serial.print(" :PotValue: ");
     Serial.print(potValue);
     Serial.print(" :motorSpeed: ");
     Serial.println(motorSpeed);
 
-    // Rotate CW slowly
     myStepper.setSpeed(motorSpeed);
-    myStepper.step(steps);
-    steps = steps + 1;
-    // delay(100);
+    if (buttonState == HIGH)
+    {
+        myStepper.step(-steps);
+        // steps = steps + 1;
+    }
+    else
+    {
+        myStepper.step(steps);
+        // steps = steps + 1;
+    }
 
-    // // Rotate CCW quickly
-    // myStepper.setSpeed(motorSpeed);
-    // myStepper.step(-stepsPerRevolution);
-    // delay(100);
+    steps = steps + 1;
 }
 
-// void speedChange()
-// {
-//     motorSpeed = map(analogRead(A0), 0, 712, 200, 1000);
-// }
+void directionChange()
+{
+    if (buttonState == 1)
+    {
+        buttonState = 0;
+    }
+    else if (buttonState == 0)
+    {
+        buttonState = 1;
+    }
+}
